8daf6b379346d222a5a0c23eee9ffa8a322e121b
Kofi <liukefei@pku.edu.cn>
2018-07-07 20:40:23 +0800

    生成随机种子配置帧

---
+++ config/getRandonSeedConfigFrame.py
@@ -0,0 +1,16 @@
+import numpy as np
+import time
+
+#生成64bit的配置帧
+frameAppended = '0000000000000000000'
+frameTitle = '000000'
+chipAndCoreAddr = ['00000000000000000000','00000000000000100000']
+with open('./config/randomSeed64bit.txt','w') as randomSeedConfigFile:
+    for addr in chipAndCoreAddr:
+        coreYAddr = addr[15:20]
+        for i in range(4):
+            data = np.random.randint(0,(1<<14)-1)
+            randomSeedConfigFile.write(frameAppended + coreYAddr + frameTitle + addr + '{:0>14b}'.format(data)+'\n')
+            time.sleep(0.001)
+        data = np.random.randint(0,(1<<8)-1)
+        randomSeedConfigFile.write(frameAppended + coreYAddr + frameTitle + addr + '{:0>8b}'.format(data)+'000000\n')
